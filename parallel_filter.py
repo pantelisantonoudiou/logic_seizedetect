7aa6381c3ce3f020ae671cd6d91e468566d6cdb2
pantelis <pantelis71@hotmail.com>
2020-07-30 17:31:28 -0400

    Update parallel_filter.py

--- parallel_filter.py
+++ parallel_filter.py
@@ -9,6 +9,18 @@ import time
 import numpy as np
 from multiprocessing import Pool
 from numba import jit, prange
+import matplotlib.pyplot as plt
+
+# def f_basic(x):
+#     return x*x
+    
+# @timing
+# def f_parallel(array):    
+#     s = 0
+#     p = Pool()
+#     s = p.map(f_basic, array)
+#     return s
+
 
 def timing(f):
     def wrap(*args, **kwargs):
@@ -21,80 +33,71 @@ def timing(f):
     return wrap
 
 
-@timing
-def f_serial(array):
+# @timing
+def f_serial_cpython(array):
     s = 0
     for i in range(array.shape[0]):
         s += array[i]*array[i]
     return s
 
-@jit(nopython = True, nogil = False) 
-def f_basic(x):
-    x*x
-    
-@timing
-def f_parallel(array):    
-    s = 0
-    p = Pool()
-    s = p.map(f_basic, array)
-    return s
-
-@timing       
-@jit(nopython = True, nogil = False)        
+# @timing       
+@jit(nopython = True, nogil = False, cache = True)        
 def f_serial_numba(array):
     s = 0
     for i in range(array.shape[0]):
         s += array[i]*array[i]
     return s
 
-@timing
-@jit(nopython = True, parallel = True, nogil = False)        
+# @timing
+@jit(nopython = True, parallel = True, nogil = False, cache = True)        
 def f_parallel_numba(array):
     s = 0
     for i in prange(array.shape[0]):
         s += array[i]*array[i]
     return s
 
-@timing
-@jit(nopython = True, parallel = True, nogil = True)        
+# @timing
+@jit(nopython = True, parallel = True, nogil = True, cache = True)        
 def f_parallel_numba_nogil(array):
     s = 0
     for i in prange(array.shape[0]):
         s += array[i]*array[i]
-    return s      
+    return s
 
 
 if __name__ == '__main__':
     
+    # vector size list
+    vector_size = np.array([10**x for x in range(10)])
     
-    # print(p.map(f, array))
-  
+    # function list
+    func_array = [f_serial_cpython, f_serial_numba, f_parallel_numba, f_parallel_numba_nogil]
     
-    array = np.random.rand(1000000)
-    f_serial(array)
-    f_parallel(array)
-    f_serial_numba(array)
-    f_parallel_numba(array)
-    f_parallel_numba_nogil(array)
+    # get function names
+    function_labels = [f.__name__ for f in func_array]
     
+    # create empty array
+    exec_time = np.zeros((len(vector_size), len(func_array)))
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    # number of repetitions
+    reps = 10;  
+                            
+    for i in range(len(vector_size)): # iterate over increasing array size
+        array = np.random.rand(vector_size[i])
+        
+        for ii in range(len(func_array)): # iterate over different function impementations
+        
+            toc = 0 # zero the timer
+            for iii in range(reps):
+                tic = time.time()
+                s = func_array[ii](array)
+                toc += time.time() - tic
+            exec_time[i,ii] = toc
+                
+
+plt.plot(np.log(vector_size[3:]), exec_time[3:])
+plt.legend(function_labels, loc = 'upper left')
+plt.ylabel('Time (ms)')
+plt.xlabel('Log input Size')
+
+
