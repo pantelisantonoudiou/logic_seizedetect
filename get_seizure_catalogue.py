b2f1fec89fabb51f09cdc08871c6863869c66bad
pantelis <pantelis71@hotmail.com>
2020-08-10 11:10:30 -0400

    create time bins and convert to headers

--- get_seizure_catalogue.py
+++ get_seizure_catalogue.py
@@ -62,9 +62,19 @@ def folder_loop(folder_path):
     feature_labels += [x.__name__  for x in cross_ch_param_list]
     feature_labels = np.array(feature_labels)
     
-    # create dataframe
-    columns = ['exp_id', 'szr_start','szr_end' , 'before_szr', 'during_szr','after_szr','szr_percentile','x_sdevs']
-    df = pd.DataFrame(data= np.zeros((0,len(columns))), columns = columns, dtype=np.int64)
+    # define time bins
+    time_bins = np.array([[-120, -90],
+                         [-90, -60], 
+                         [-60, -30], 
+                         [-30, 0],
+                         [0, 30],
+                         [30, 60],
+                         [60, 90],
+                         [90, 120]
+                         ])
+    time_cols = [] # convert time bins to headers
+    for x in time_bins.tolist():
+        time_cols.append('_'.join(map(str, x)))
 
     for i in tqdm(range(0,len(filelist))): # loop through experiments   len(filelist)
 
@@ -74,7 +84,6 @@ def folder_loop(folder_path):
         
         ## UNCOMMENT LINE BELOW TO : Clean and filter data
         # data = preprocess_data(data,  clean = True, filt = True, verbose = 0)
-        # print('-> data pre-processed.')
         
         # Get features and labels
         x_data, labels = get_features_allch(data,param_list,cross_ch_param_list)
@@ -88,6 +97,10 @@ def folder_loop(folder_path):
         
         # GET multifeatures????
         for ii in range(1): # iterate through parameteres  x_data.shape[1] len(feature_labels) 
+        
+            # create dataframe
+            columns = ['exp_id', 'szr_start', 'szr_end', 'szr_percentile','x_sdevs', 'before_szr', 'during_szr','after_szr']
+            df = pd.DataFrame(data= np.zeros((0,len(columns))), columns = columns, dtype=np.int64)
 
             # get seizure index
             bounds_true = find_szr_idx(y_true, np.array([0,1])) # true
@@ -95,7 +108,7 @@ def folder_loop(folder_path):
             if bounds_true.shape[0] > 0:
         
                 # get seizure and surround properties
-                szrs = get_surround(x_data[:,6], bounds_true, np.array([30, 120]))
+                szrs = get_surround(x_data[:,6], bounds_true, time_bins)
                 df_temp = pd.DataFrame(data = szrs, columns = ['before_szr', 'during_szr','after_szr','szr_percentile','x_sdevs'])
                 
                 # insert seizure start and end
@@ -106,14 +119,17 @@ def folder_loop(folder_path):
                 df_temp.insert(0, 'exp_id', [filelist[i]] * bounds_true.shape[0])
                 
                 # append to dataframe
+    
                 df = df.append(df_temp)
+                
+                # df_temp.to_csv(labels[i] +'.csv', mode='a', header=True, index = False) # ADD CORRECT NAME
     return df
 
 # @jit(nopython = True)
 from scipy.stats import percentileofscore
-def get_surround(feature, idx, surround_time):
+def get_surround(feature, idx, time_bins):
     """
-    get_surround(feature, idx, surround_time)
+    get_surround(feature, idx, time_bins)
 
     Parameters
     ----------
@@ -127,24 +143,23 @@ def get_surround(feature, idx, surround_time):
 
     """
     
-    outbins = surround_time/win # convert bounds to bins
-    outbins = outbins.astype(np.int64) # convert to integer
+    # convert time bins to window bins
+    time_bins = time_bins/win
+    time_bins = time_bins.astype(np.int64) # convert to integer
     
     # create empty vectors to store before, after and within seizure features
     szrs = np.zeros((idx.shape[0],5))
     
-    # get percentile of during szr vs rest of feature
-    
     for i in range(idx.shape[0]): # iterate over seizure number
 
-        # get feature before withing and after seizure
-        szrs[i,0] = np.mean(feature[idx[i,0] - outbins[1]: idx[i,0] - outbins[0]]) # bef
-        szrs[i,1] = np.mean(feature[idx[i,0]:idx[i,1]]) # during
-        szrs[i,2] = np.mean(feature[idx[i,1] + outbins[0]: idx[i,1] + outbins[1]]) # after
-   
-        remapped_array = remap_array(np.sort(feature)) # remap array from 0 to 100
-        szrs[i,3] = percentileofscore (remapped_array, szrs[i,1]) # get percentile
-        szrs[i,4] = ( szrs[i,1]-np.mean(feature))/np.std(feature) # get deviations from mean
+        # # get feature before withing and after seizure
+        # szrs[i,0] = np.mean(feature[idx[i,0] - outbins[1]: idx[i,0] - outbins[0]]) # bef
+        # szrs[i,1] = np.mean(feature[idx[i,0]:idx[i,1]]) # during
+        # szrs[i,2] = np.mean(feature[idx[i,1] + outbins[0]: idx[i,1] + outbins[1]]) # after
+        
+        breakpoint()
+        szrs[i,3] = percentileofscore (feature, szrs[i,1]) # get percentile
+        szrs[i,4] = (szrs[i,1]-np.mean(feature))/np.std(feature) # get deviations from mean
         
     return szrs
 
@@ -154,6 +169,7 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
+#  remapped_array = remap_array(np.sort(feature)) # remap array from 0 to 100
 @jit(nopython = True)
 def remap_array(array):
     min_value = array.min()
