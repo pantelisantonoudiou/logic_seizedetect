7ddcd39f1e023a1361b6cb1b9b8ac47d87d75099
pantelis <pantelis71@hotmail.com>
2020-08-10 17:59:47 -0400

    update paths

--- get_seizure_catalogue.py
+++ get_seizure_catalogue.py
@@ -39,7 +39,7 @@ def multi_folder(main_path):
         print('Analyzing', folders[i], '...' )
         
         # get dataframe with detected seizures
-        df_temp = folder_loop(os.path.join(main_path,folders[i]))
+        df_temp = folder_loop(main_path,folders[i])
         
         if i == 0: # create copy of the dataframe
             df = df_temp.copy()
@@ -49,10 +49,10 @@ def multi_folder(main_path):
     return df
 
 
-def folder_loop(folder_path):
+def folder_loop(main_path,folder_path):
     
     # get file list 
-    ver_path = os.path.join(folder_path, 'verified_predictions_pantelis')
+    ver_path = os.path.join(main_path,folder_path, 'verified_predictions_pantelis')
     filelist = list(filter(lambda k: '.csv' in k, os.listdir(ver_path))) # get only files with predictions
     filelist = [os.path.splitext(x)[0] for x in filelist] # remove csv ending
     
@@ -84,7 +84,7 @@ def folder_loop(folder_path):
     for i in tqdm(range(0,len(filelist))): # loop through experiments   len(filelist)
 
         # get data and true labels
-        data, y_true = get_data(folder_path,filelist[i], ch_num = num_channels, 
+        data, y_true = get_data(os.path.join(main_path,folder_path),filelist[i], ch_num = num_channels, 
                                 inner_path={'data_path':'filt_data', 'pred_path':'verified_predictions_pantelis'} , load_y = True)
         
         ## UNCOMMENT LINE BELOW TO : Clean and filter data
@@ -100,7 +100,6 @@ def folder_loop(folder_path):
         for ii in range(1): # iterate through parameteres  x_data.shape[1] len(feature_labels) 
         
             # create dataframe
-            columns = ['exp_id', 'szr_start', 'szr_end', 'szr_percentile','x_sdevs', 'before_szr', 'during_szr','after_szr']
             df = pd.DataFrame(data= np.zeros((0,len(columns))), columns = columns, dtype=np.int64)
 
             # get seizure index
@@ -109,21 +108,16 @@ def folder_loop(folder_path):
             if bounds_true.shape[0] > 0:
         
                 # get seizure and surround properties
-                szrs = get_surround(x_data[:,6], bounds_true, time_bins)
-                df_temp = pd.DataFrame(data = szrs, columns = ['before_szr', 'during_szr','after_szr','szr_percentile','x_sdevs'])
+                szrs = get_surround(x_data[:,ii], bounds_true, time_bins)
                 
                 # insert seizure start and end
-                df_temp.insert(0, 'szr_end', bounds_true[:,1])
-                df_temp.insert(0, 'szr_start', bounds_true[:,0])
-                
-                # insert exp ids
-                df_temp.insert(0, 'exp_id', [filelist[i]] * bounds_true.shape[0])
+                df['exp_id'] = [os.path.join(folder_path,filelist[i])] * bounds_true.shape[0]
+                df['szr_start'] = bounds_true[:,0]
+                df['szr_end'] =  bounds_true[:,1]
+
                 
                 # append to dataframe
-    
-                df = df.append(df_temp)
-                
-                # df_temp.to_csv(labels[i] +'.csv', mode='a', header=True, index = False) # ADD CORRECT NAME
+                df_temp.to_csv(labels[i] +'.csv', mode='a', header=True, index = False) # ADD CORRECT NAME
     return df
 
 # @jit(nopython = True)
@@ -149,19 +143,27 @@ def get_surround(feature, idx, time_bins):
     time_bins = time_bins.astype(np.int64) # convert to integer
     
     # create empty vectors to store before, after and within seizure features
-    szrs = np.zeros((idx.shape[0],5))
+    fixed_bins = 3
+    szrs = np.zeros((idx.shape[0],len(time_bins) + fixed_bins))
     
     for i in range(idx.shape[0]): # iterate over seizure number
-
-        # # get feature before withing and after seizure
-        # szrs[i,0] = np.mean(feature[idx[i,0] - outbins[1]: idx[i,0] - outbins[0]]) # bef
-        # szrs[i,1] = np.mean(feature[idx[i,0]:idx[i,1]]) # during
-        # szrs[i,2] = np.mean(feature[idx[i,1] + outbins[0]: idx[i,1] + outbins[1]]) # after
         
-        breakpoint()
-        szrs[i,3] = percentileofscore (feature, szrs[i,1]) # get percentile
-        szrs[i,4] = (szrs[i,1]-np.mean(feature))/np.std(feature) # get deviations from mean
+        szr_feature = np.mean(feature[idx[i,0]:idx[i,1]]) # during
+        szrs[i,0] = percentileofscore (feature, szr_feature) # get percentile
+        szrs[i,1] = (szr_feature-np.mean(feature))/np.std(feature) # get deviations from mean  
+        szrs[i,2] = szr_feature
         
+        for ii in range(time_bins.shape[0]): # get mean of each time bin
+            if time_bins[ii,0] < 0:
+                idx_start =  idx[i,0] + time_bins[ii,0];
+                idx_end = idx[i,0] + time_bins[ii,1];
+            else:
+                idx_start =  idx[i,1] + time_bins[ii,0];
+                idx_end = idx[i,1] + time_bins[ii,1];
+            print(idx_start, idx_end)
+            
+            szrs[i,ii+fixed_bins] = np.mean(feature[idx_start : idx_end])
+            
     return szrs
 
 @jit(nopython = True)
