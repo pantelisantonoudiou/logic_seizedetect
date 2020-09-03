569bafdd8394929d4397b8647115fc37c84119a0
pantelis <pantelis71@hotmail.com>
2020-09-03 17:53:30 -0400

    Create get_common_methods.py

---
+++ get_common_methods.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 09:00:40 2020
+
+@author: Pante
+"""
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+paths = [r'C:\Users\Pante\Desktop\seizure_data_tb\Train_data\model_performance\best_method_metrics.csv',
+         r'C:\Users\Pante\Desktop\seizure_data_tb\Test_data\model_performance\best_method_metrics.csv'
+         ]
+         
+df = pd.read_csv(paths[0])
+col_idx = df.columns.str.contains('Enabled')
+cols = df.columns[col_idx]
+cols = np.array([x.replace('Enabled_', '') for x in cols])
+
+# f1, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+idx = np.zeros((len(df),len(paths)), dtype=bool)
+for i in range(0,len(paths)):
+    df = pd.read_csv(paths[i])
+    # ax1.plot(df['detected_ratio'])
+    # ax2.plot(df['false_positives'])
+    idx[:,i] = np.array((df['detected_ratio'] > 0.99) & (df['false_positives'] < 700))
+     
+
+# get common index    
+common_idx = np.where(np.logical_and(idx[:,0], idx[:,1]))[0]
+for i in range(common_idx.shape[0]): 
+    enabled_idx = np.array(df.loc[common_idx[i]][col_idx]).astype(bool)
+    print(cols[enabled_idx])
+
+# get false positives    
+df_list = []    
+
+for i in range(0,len(paths)):
+    df = pd.read_csv(paths[i])
+    df_list.append(df['false_positives'][common_idx])
+    plt.plot(df_list[i])
+# print(df_list[0], df_list[1])
+# df_best = df.loc[common_idx]
