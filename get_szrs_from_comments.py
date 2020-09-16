e3a124c378128f2f68bfa704cb2e265988176868
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2020-09-16 18:31:08 -0400

    add filter comments

--- get_szrs_from_comments.py
+++ get_szrs_from_comments.py
@@ -10,7 +10,7 @@ Created on Wed Sep 16 15:00:13 2020
 
 # --->>>>>>>>> 
 # Add path to raw data folder in following format -> r'PATH'
-input_path = r'C:\Users\panton01\Desktop\Trina-seizures\3642_3641_3560_3514\raw_data'
+input_path = r'W:\Maguire Lab\Trina\2020\06- June\5142_5143_5160_5220\raw_data'
                 # ---<<<<<<<< #
 
 ### ------------------------------------------------------------------------###
@@ -19,23 +19,19 @@ input_path = r'C:\Users\panton01\Desktop\Trina-seizures\3642_3641_3560_3514\raw_
 import adi,os,sys,tables,json
 from tqdm import tqdm
 import numpy as np
-from scipy import signal
-from math import floor
+from multich_dataPrep import lab2mat
 from string import ascii_lowercase
 from path_helper import get_dir,sep_dir,rem_array
 ### ------------------------------------------------------------------------###
 
 
-## print animal to be analyzed ##
-
-# CLASS FOR CONERTING LABCHART TO H5 FILES
-class lab2mat:
+# CLASS FOR RETRIEVING SEIZURE COMMENTS FROM LABCHART
+class SzrsFromLab:
     """
-    Class for convering labchart files to H5 format
+    Class for retrieving seizure comments from lachart
     One labchart file may contain recordings from multiple animals
-    Each animal may have multiple channels
+    Each animal may encompass multiple channels
     For example one file-> 1-12 channels and 4 animals
-    One H5 file will contain all channels for 1 animal (eg. 1-3)
     
     -> __init__: constructor to get instance attributes
     -> mainfunc: function that iterates over labchart files
@@ -45,23 +41,21 @@ class lab2mat:
     -> load: load object properties from dict (stored in json format)
     """
    
-    # class constructor (data retrieval)
-    # class constructor (data retrieval)
+    # class constructor (load_path retrieval)
     def __init__(self, input_path):
         """
-        lab2mat(main_path)
+        SzrsFromLab(main_path)
 
         Parameters
         ----------
-        input_path : STRING
-            Raw data path.
+        input_path : Str, Raw data path.
 
         """
         # pass input path
-        self.input_path = input_path
+        self.load_path = input_path
 
         # Get general and inner paths
-        self.gen_path, innerpath = sep_dir(input_path,1)
+        self.gen_path, innerpath = sep_dir(self.load_path,1)
         
         # load object properties as dict
         obj_props = lab2mat.load(os.path.join(self.gen_path, 'organized.json'))
@@ -81,8 +75,8 @@ class lab2mat:
         """
         
         # make path
-        if os.path.exists(self.save_path) is False:
-            os.mkdir(self.save_path)
+        # if os.path.exists(self.save_path) is False:
+        #     os.mkdir(self.save_path)
         
         # init progress bar
         total = len(self.filelist)*len(self.animal_ids)       
@@ -98,16 +92,14 @@ class lab2mat:
             ch_idx = np.linspace(1,f.n_channels,f.n_channels,dtype=int)
             ch_list = np.split(ch_idx, round(f.n_channels/len(self.ch_struct)))
             
+            
             if len(ch_list) - len(self.animal_ids) != 0:
                 print('Animal numbers do not match channel structure')
                 
             for ii in range(len(ch_list)): # iterate through animals
-                
-                # get exp name
-                filename = self.filelist[i].replace(self.file_ext, "") + '_' + self.animal_ids[ii]
          
-                # downsample and save in chuncks
-                self.save_chunks(f,filename,ch_list[ii])
+                # extract and save comments
+                self.save_chunks(f,self.animal_ids[ii],ch_list[ii])
                 
                 print(cntr, 'of',total ,'Experiments Saved')
                 cntr += 1 # update counter
@@ -130,7 +122,7 @@ class lab2mat:
         None.
 
         """
-        
+        breakpoint()
         ch_list = ch_list - 1 # convert channel to python format
         all_blocks = len(file_obj.channels[0].n_samples) # get all blocks
         
@@ -148,6 +140,7 @@ class lab2mat:
             # get comments
             user_coms = file_obj.records[block].comments
             
+            
             ### SAVING PARAMETERS ##
             file_id  = filename + ascii_lowercase[block] + '.h5' # add extension
             full_path = os.path.join(self.save_path, file_id) # get full save path
@@ -155,10 +148,35 @@ class lab2mat:
             for i in range(len(ch_list)): ## Iterate over all animal channels ##
                 # get channel obj
                 chobj = file_obj.channels[ch_list[ii]] 
+                # filter coms
+                coms,com_idx = filter_coms(user_coms, ch_list[0])
                     
 
     
-    def filter_coms(comments, channel):
+    def filter_coms(comments, channel_id):
+        """
+        filter_coms(comments, channel_id)
+
+        Parameters
+        ----------
+        comments : list of comments objects
+        channel_id : Int, channel numner
+
+        Returns
+        -------
+        coms: np array.
+        com_time : ndarray, comment times.
+
+        """
+        coms = np.array([]) # create comment array
+        com_time =  np.array([]) # create comment time array in seconds
+        
+        for c in comments: # iterate comments
+            if c.channel_ == channel_id: # if comment channel matches channel number
+                coms = np.append(coms, c.text) # append com
+                com_time = np.append(com_time, c.time) # append com time
+                
+        return np.core.defchararray.lower(coms), com_time
         
         
 
@@ -166,7 +184,7 @@ class lab2mat:
 # # Execute if module runs as main program
 # if __name__ == '__main__':
 #    # create instance
-#    obj = lab2mat(input_path)
+#    obj = SzrsFromLab(input_path)
    
 #    # run analysis
 #    obj.mainfunc()
