514bc4a18feda352a03a8ce7d2a52c1a20bf6118
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2020-09-21 16:17:18 -0400

    add non detected seizure report

--- user_model_compare.py
+++ user_model_compare.py
@@ -53,12 +53,12 @@ def main_func(main_path):
             
             # get matching seizures
             df_save['total'].at[i] = true_idx.shape[0] # total
-            df_save['detected'].at[i] = get_match(true_idx, pred_bounds) # detected
+            df_save['detected'].at[i], non_detected_idx = get_match(true_idx, pred_bounds) # detected
             df_save['false_positives'].at[i] = pred_bounds.shape[0] - df_save['detected'][i] # false positives
             
             if df_save['total'][i] != df_save['detected'][i]:
                 print('not all seizures were detected')
-                print(filelist[i], true_idx, pred_bounds)
+                print(filelist[i], get_hours(non_detected_idx))
     
     # get grand totals
     df_save['total'].at[i+1] =  df_save['total'].sum()    
@@ -81,19 +81,28 @@ def get_match(true_idx, pred_bounds):
     Returns
     -------
     detected : Int, Number of detected seizures from true idx
+    non_detected_idx: ndarray flat, index of non matched seizures
 
     """
-    
+
     detected = 0 # set detected to 0
+    detected_idx = np.array([],dtype=np.int64) # init empty array
     
-    for i in range(pred_bounds.shape[0]): # iterate through true seizures
-    
-        # check if idx true is between predictions low and upper bounds
-        if any((pred_bounds[i,0] < true_idx) & (pred_bounds[i,1] > true_idx)):
-            
-            detected += 1 # increase matching by 1
+    # get all_idx
+    all_idx = np.linspace(0,true_idx.shape[0]-1,true_idx.shape[0], dtype=np.int64)
+    for i in range(pred_bounds.shape[0]): # iterate through pred seizures
         
-    return detected          
+        # find if true idx is between predictions low and upper bounds
+        idx = (pred_bounds[i,0] < true_idx) & (pred_bounds[i,1] > true_idx)
+        
+        if any(idx): # if detected add 1
+            detected += 1 
+            detected_idx = np.append(detected_idx, np.where(idx))  # append to detected idx
+    
+    # get only non detected index
+    non_detected_idx = np.delete(all_idx, detected_idx)
+        
+    return detected, true_idx[non_detected_idx] 
 
     
 def get_szr_index(df, exp_id):
@@ -123,7 +132,28 @@ def get_szr_index(df, exp_id):
     idx = idx[np.logical_not(np.isnan(idx))]
     
     return idx
-    
+   
+   
+def get_hours(seconds_list):
+    """
+    get_hours(seconds_list)
+
+    Parameters
+    ----------
+    seconds_list : seconds array
+
+    Returns
+    -------
+    str1 : list with times in hour format
+
+    """
+    str1 = []
+    for seconds in list(seconds_list):
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        seconds = seconds % 60
+        str1.append('{}:{}:{}'.format(int(hours), int(minutes), int(seconds)))
+    return (str1) 
     
 # Execute if module runs as main program
 if __name__ == '__main__':
