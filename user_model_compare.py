bc30ee236d7acd838a588b6751f07db3a395df07
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2020-09-21 15:24:59 -0400

    first working version

--- user_model_compare.py
+++ user_model_compare.py
@@ -8,6 +8,7 @@ Created on Fri Sep 18 13:58:54 2020
 import os
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from array_helper import find_szr_idx, merge_close
 from multich_dataPrep import lab2mat
 
@@ -16,6 +17,9 @@ main_path = r'W:\Maguire Lab\Trina\2020\06- June\5142_5143_5160_5220'
 
 
 def main_func(main_path):
+    
+    # dict load
+    settings = lab2mat.load(os.path.join(main_path, 'organized.json'))
     
     # get user seizures
     df = pd.read_csv(os.path.join(main_path, 'Extracted_seizures.csv'), header = None)
@@ -26,14 +30,17 @@ def main_func(main_path):
     
     if len(df) != len(filelist): # file check
         print('Warning: length of extracted seizures does not match list of verified predictions!')
+    print(str(len(filelist)) + ' files will be analyzed...')
     
     # create dataframe to store metrics
-    df_save = pd.Dataframe(np.zeros(len(df),3), columns = ['tota'])
+    df_save = pd.DataFrame(np.zeros((len(df)+1,3)), columns = ['total', 'detected', 'false_positives'])
+    df_save.insert(0, 'exp_id', filelist + ['Grand_sum'])
     
-    for i in range(len(filelist)):
+
+    for i in tqdm(range(len(filelist))):
         
         # get user scored seizured index (gold standard)
-        idx = get_szr_index(df, filelist[i].replace('.csv',''))
+        true_idx = get_szr_index(df, filelist[i].replace('.csv',''))
         
         # load seizure index from user-curated model-predictions 
         rawpred = np.loadtxt(os.path.join(ver_path, filelist[i]), delimiter=',', skiprows=1)
@@ -42,17 +49,52 @@ def main_func(main_path):
 
             # get index bounds of semi-manual detected seizures
             pred_bounds = find_szr_idx(rawpred, np.array([0,1]))
+            pred_bounds *= settings['win'] # convert to seconds
             
             # get matching seizures
-            idx.shape[0] # total
-            detected = get_match(szr_idx, pred_bounds) # detected
+            df_save['total'].at[i] = true_idx.shape[0] # total
+            df_save['detected'].at[i] = get_match(true_idx, pred_bounds) # detected
+            df_save['false_positives'].at[i] = pred_bounds.shape[0] - df_save['detected'][i] # false positives
             
-          
+            if df_save['total'][i] != df_save['detected'][i]:
+                print('not all seizures were detected')
+                print(filelist[i], true_idx, pred_bounds)
+    
+    # get grand totals
+    df_save['total'].at[i+1] =  df_save['total'].sum()    
+    df_save['detected'].at[i+1] =  df_save['detected'].sum()
+    df_save['false_positives'].at[i+1] =  df_save['false_positives'].sum() 
+    
+    # save csv
+    df_save.to_csv(os.path.join(main_path, 'detected.csv'),index=False)
+    print('Metrics for seizure matching completed')    
         
-def get_match(szr_idx, pred_bounds):
-    for i in range(idx_true.shape[0]):
+def get_match(true_idx, pred_bounds):
+    """
+    get_match(true_idx, pred_bounds)
+
+    Parameters
+    ----------
+    true_idx : ndarray flat, index of true seizures
+    pred_bounds : 2D ndarray, semi-automated seizure bounds (rows = sezuires, cols = [start, stop])
+
+    Returns
+    -------
+    detected : Int, Number of detected seizures from true idx
+
+    """
+    
+    detected = 0 # set detected to 0
+    
+    for i in range(pred_bounds.shape[0]): # iterate through true seizures
+    
+        # check if idx true is between predictions low and upper bounds
+        if any((pred_bounds[i,0] < true_idx) & (pred_bounds[i,1] > true_idx)):
+            
+            detected += 1 # increase matching by 1
         
-    return matching          
+    return detected          
+
     
 def get_szr_index(df, exp_id):
     """
