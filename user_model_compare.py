da54583647e23f09a1023e6e766951a7a9a2272b
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2020-09-21 14:33:04 -0400

    Update user_model_compare.py

--- user_model_compare.py
+++ user_model_compare.py
@@ -9,6 +9,7 @@ import os
 import numpy as np
 import pandas as pd
 from array_helper import find_szr_idx, merge_close
+from multich_dataPrep import lab2mat
 
 # get main path
 main_path = r'W:\Maguire Lab\Trina\2020\06- June\5142_5143_5160_5220'
@@ -26,22 +27,32 @@ def main_func(main_path):
     if len(df) != len(filelist): # file check
         print('Warning: length of extracted seizures does not match list of verified predictions!')
     
+    # create dataframe to store metrics
+    df_save = pd.Dataframe(np.zeros(len(df),3), columns = ['tota'])
+    
     for i in range(len(filelist)):
         
-        # get user scored seizured index
+        # get user scored seizured index (gold standard)
         idx = get_szr_index(df, filelist[i].replace('.csv',''))
         
-        # load predictions
+        # load seizure index from user-curated model-predictions 
         rawpred = np.loadtxt(os.path.join(ver_path, filelist[i]), delimiter=',', skiprows=1)
         
-        if np.sum(rawpred)>0:
-            breakpoint()
-            # get index bounds of semi-manual detected seizures
-            bounds_pred = find_szr_idx(rawpred, [0,1]) ##### - ERROR !!!!!!!!!!!!!!!!!
+        if np.sum(rawpred)>0: # check if any seizures were detected
 
-            print(bounds_pred)
-        
+            # get index bounds of semi-manual detected seizures
+            pred_bounds = find_szr_idx(rawpred, np.array([0,1]))
+            
+            # get matching seizures
+            idx.shape[0] # total
+            detected = get_match(szr_idx, pred_bounds) # detected
+            
           
+        
+def get_match(szr_idx, pred_bounds):
+    for i in range(idx_true.shape[0]):
+        
+    return matching          
     
 def get_szr_index(df, exp_id):
     """
