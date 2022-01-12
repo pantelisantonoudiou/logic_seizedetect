9298ddda589deb1d99fcdd5131b559397faa1a5c
Pantelis Antonoudiou <pantelis71@hotmail.com>
2022-01-12 10:31:06 -0500

    Update model_select.py

--- model_select.py
+++ model_select.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-from model_selection.test_models import MethodTest
+# from model_selection.model_metrics_train import MethodTest
+from model_selection.model_metrics_test import MethodTest
 
-path = r'\\SUPERCOMPUTER2\Shared\seizure_detect_data\train'
+path = r'\\SUPERCOMPUTER2\Shared\seizure_detect_data\test'
 obj = MethodTest(path)
 obj.multi_folder() # get catalogue for multiple folders
